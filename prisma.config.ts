06073ab44435f2dd95353caddfa77f0d51b65508
pawan suthar <pawansuthar187@gmail.com>
2026-08-21 11:39:10 +0530

    prisma schema update-3

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,7 +4,7 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "./prisma/project.prisma",
+  schema: "./prisma/models/project.prisma",
   migrations: {
     path: "prisma/migrations",
   },
